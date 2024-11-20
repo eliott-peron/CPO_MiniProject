c18adcbce1390651f4047b609db1e4d29a69b5b1
leoboree <leoboree@10.3.61.173>
2024-11-20 11:08:43 +0100

    Démineur

--- demineur_1_peron/src/demineur_1_peron/Demineur_1_peron.java
+++ demineur_1_peron/src/demineur_1_peron/Demineur_1_peron.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package demineur_1_peron;
-
+import demineur_1_peron.Cellule ;
 /**
  *
  * @author eliot
@@ -15,6 +15,32 @@ public class Demineur_1_peron {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cellule cellule1 = new Cellule();
+        System.out.println("État initial de la cellule : " + cellule1.toString()); // Devrait afficher "?"
+
+        // Placer une bombe
+        cellule1.placerBombe();
+        System.out.println("Après avoir placé une bombe : " + cellule1.toString()); // Toujours "?" car non dévoilée
+
+        // Révéler la cellule avec une bombe
+        cellule1.revelerCellule();
+        System.out.println("Cellule après révélation (avec bombe) : " + cellule1.toString()); // Devrait afficher "B"
+
+        // Création d'une nouvelle cellule sans bombe
+        Cellule cellule2 = new Cellule();
+        cellule2.setNbBombesAdjacentes(3); // Définir 3 bombes adjacentes
+        System.out.println("\nNouvelle cellule avant révélation : " + cellule2.toString()); // Devrait afficher "?"
+
+        // Révéler la cellule avec bombes adjacentes
+        cellule2.revelerCellule();
+        System.out.println("Nouvelle cellule après révélation : " + cellule2.toString()); // Devrait afficher "3"
+
+        // Création d'une cellule vide sans bombe ni bombes adjacentes
+        Cellule cellule3 = new Cellule();
+        cellule3.revelerCellule();
+        System.out.println("\nCellule vide dévoilée : " + cellule3.toString()); // Devrait afficher " "
+    
+
     }
     
 }
