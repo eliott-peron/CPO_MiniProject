2fcfb89d97ecf3c643bc2c1a26aa2011a761eb4d
eliott  peron <eliott.peron@epfedu.fr>
2024-11-20 11:50:17 +0100

    demineur classe cellule et grilledejeux sont terminés

--- demineur_1_peron/src/demineur_1_peron/Demineur_1_peron.java
+++ demineur_1_peron/src/demineur_1_peron/Demineur_1_peron.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package demineur_1_peron;
-import demineur_1_peron.Cellule ;
+
+import demineur_1_peron.Cellule;
+
 /**
  *
  * @author eliot
@@ -14,33 +16,56 @@ public class Demineur_1_peron {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Création d'une première cellule (par défaut : pas de bombe, non dévoilée)
         Cellule cellule1 = new Cellule();
-        System.out.println("État initial de la cellule : " + cellule1.toString()); // Devrait afficher "?"
+        System.out.println("Etat initial de la cellule : " + cellule1.toString()); // Attendu : "?"
 
-        // Placer une bombe
+        // Placer une bombe sur cette cellule
         cellule1.placerBombe();
-        System.out.println("Après avoir placé une bombe : " + cellule1.toString()); // Toujours "?" car non dévoilée
+        System.out.println("Apres avoir place une bombe : " + cellule1.toString()); // Attendu : "?" (non dévoilée)
 
-        // Révéler la cellule avec une bombe
+        // Révéler la cellule (avec bombe)
         cellule1.revelerCellule();
-        System.out.println("Cellule après révélation (avec bombe) : " + cellule1.toString()); // Devrait afficher "B"
+        System.out.println("Cellule apres revelation (avec bombe) : " + cellule1.toString()); // Attendu : "B"
 
         // Création d'une nouvelle cellule sans bombe
         Cellule cellule2 = new Cellule();
-        cellule2.setNbBombesAdjacentes(3); // Définir 3 bombes adjacentes
-        System.out.println("\nNouvelle cellule avant révélation : " + cellule2.toString()); // Devrait afficher "?"
+        cellule2.setNbBombesAdjacentes(3); // Indiquer 3 bombes adjacentes
+        System.out.println("\nNouvelle cellule avant revelation : " + cellule2.toString()); // Attendu : "?"
 
-        // Révéler la cellule avec bombes adjacentes
+        // Révéler la cellule avec des bombes adjacentes
         cellule2.revelerCellule();
-        System.out.println("Nouvelle cellule après révélation : " + cellule2.toString()); // Devrait afficher "3"
+        System.out.println("Nouvelle cellule apres revelation : " + cellule2.toString()); // Attendu : "3"
 
-        // Création d'une cellule vide sans bombe ni bombes adjacentes
+        // Création d'une cellule vide (pas de bombe et 0 bombes adjacentes)
         Cellule cellule3 = new Cellule();
-        cellule3.revelerCellule();
-        System.out.println("\nCellule vide dévoilée : " + cellule3.toString()); // Devrait afficher " "
-    
+        cellule3.revelerCellule(); // Révéler la cellule vide
+        System.out.println("\nCellule vide devoilee : " + cellule3.toString()); // Attendu : " "
 
+        // Affichage final pour valider le comportement des cellules
+        System.out.println("\nTests termines !");
+
+        GrilleDeJeu grille = new GrilleDeJeu(5, 5, 5);
+
+        // Affichage de la grille initiale (toutes les cellules sont non dévoilées)
+        System.out.println("Grille initiale :");
+        System.out.println(grille.toString());
+
+        // Révélation d'une cellule spécifique (par exemple, la cellule en (2,2))
+        System.out.println("Revelation de la cellule (2,2) :");
+        grille.revelerCellule(2, 2);
+
+        // Affichage de la grille après la révélation
+        System.out.println("Grille apres la revelation :");
+        System.out.println(grille.toString());
+
+        // Vérification de la victoire
+        if (grille.toutesCellulesRevelees()) {
+            System.out.println("Felicitations ! Vous avez gagne !");
+        } else {
+            System.out.println("Continuez a jouer !");
+        }
     }
-    
 }
+
+
