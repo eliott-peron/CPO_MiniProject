ad09d77ab184e7be3f958b86cf1a0257707680c1
eliott  peron <eliott.peron@epfedu.fr>
2024-11-20 20:52:42 +0100

    demineur classe cellule,partie et grilledejeux sont terminés

--- demineur_1_peron/src/demineur_1_peron/Cellule.java
+++ demineur_1_peron/src/demineur_1_peron/Cellule.java
@@ -4,15 +4,12 @@ package demineur_1_peron;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author leoboree
  */
-
-    
-
 public class Cellule {
+
     // Attributs privés
     private boolean presenceBombe;         // Indique si la cellule contient une bombe
     private boolean devoilee;              // Indique si la cellule est dévoilée
@@ -50,19 +47,47 @@ public class Cellule {
         this.nbBombesAdjacentes = nb;
     }
 
-    // Méthode pour afficher les informations de la cellule
     @Override
     public String toString() {
         if (devoilee) {
             if (presenceBombe) {
                 return "B"; // Représente une bombe
             } else if (nbBombesAdjacentes > 0) {
-                return String.valueOf(nbBombesAdjacentes); // Affiche le nombre de bombes adjacentes
+                return getColorForNumber(nbBombesAdjacentes) + nbBombesAdjacentes + "\u001B[0m"; // Nombre coloré
             } else {
                 return " "; // Cellule vide
             }
         } else {
             return "?"; // Cellule non dévoilée
+        }
+    }
+
+    /**
+     * Retourne le code couleur ANSI selon le nombre.
+     *
+     * @param number Le nombre de bombes adjacentes.
+     * @return Un code couleur ANSI.
+     */
+    private String getColorForNumber(int number) {
+        switch (number) {
+            case 1:
+                return "\u001B[34m"; // Bleu
+            case 2:
+                return "\u001B[32m"; // Vert
+            case 3:
+                return "\u001B[31m"; // Rouge
+            case 4:
+                return "\u001B[35m"; // Magenta
+            case 5:
+                return "\u001B[33m"; // Jaune
+            case 6:
+                return "\u001B[36m"; // Cyan
+            case 7:
+                return "\u001B[30m"; // Noir
+            case 8:
+                return "\u001B[37m"; // Blanc
+            default:
+                return "\u001B[0m"; // Réinitialisation (aucune couleur)
         }
     }
 }
