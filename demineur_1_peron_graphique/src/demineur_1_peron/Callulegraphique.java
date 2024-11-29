1fe7b9ad8e6c296f695cd0d4241a343a36eb2109
leoboree <leoboree@10.3.61.173>
2024-11-29 11:08:51 +0100

    Constructeur, toString dans Callulegraphique

--- demineur_1_peron_graphique/src/demineur_1_peron/Callulegraphique.java
+++ demineur_1_peron_graphique/src/demineur_1_peron/Callulegraphique.java
@@ -4,6 +4,7 @@
  */
 package demineur_1_peron;
 
+import java.awt.Graphics;
 import javax.swing.JButton;
 
 /**
@@ -11,16 +12,22 @@ import javax.swing.JButton;
  * @author leoboree
  */
 public class Callulegraphique extends JButton {
-    int largeur;
-    int longueur;
-    Cellule celluleDMR;
-
-    public Callulegraphique(int largeur, int longueur, Cellule celluleDMR) {
-        this.largeur = largeur;
-        this.longueur = longueur;
-        this.celluleDMR = celluleDMR;
-    }
-    
-    
-    
+ int largeur; // largeur en pixel de la cellule
+ int hauteur; // hauteur en pixel de la cellule
+ Cellule celluleDMR;
+ // constructeur (appelé depuis FenetrePrincipale)
+ public Callulegraphique(Cellule celluleDMR, int l,int h) {
+ this.largeur = l;
+ this.hauteur = h;
+ this.celluleDMR = celluleDMR;
+ }
+ // Methode gérant le dessin de la cellule
+ @Override
+ protected void paintComponent(Graphics g) {
+ super.paintComponent(g);
+ this.setText(celluleDMR.toString());
+ }
 }
+    
+    
+
